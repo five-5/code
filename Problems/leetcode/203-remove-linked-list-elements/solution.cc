65bc1f0cd545bf1ff90832e1a3fa1191a1e63de7
five-5 <shenhuan1996@qq.com>
2019-03-29 13:27:54 +0800

    add recursive method

--- Problems/leetcode/203-remove-linked-list-elements/solution.cc
+++ Problems/leetcode/203-remove-linked-list-elements/solution.cc
@@ -19,7 +19,7 @@ public:
 
         // no dummy head
         // 1.deal head node
-        /*while (head != nullptr && head->val == val) {
+        while (head != nullptr && head->val == val) {
             ListNode *delNode = head;
             head = head->next;
             delete delNode;
@@ -28,7 +28,26 @@ public:
 
         if (head == nullptr) {
             return nullptr;
-        }*/
+        }
+    
+        ListNode *pre = head;
+        while (pre->next) {
+            if (pre->next->val == val) {
+                ListNode *delNode = pre->next;
+                pre->next = pre->next->next;
+                delete delNode;
+                delNode = nullptr;
+            } else {
+                pre = pre->next;
+            }
+        }
+        return head;
+    }
+
+    ListNode* removeElementsWithDummy(ListNode* head, int val) {
+        if (head == nullptr) {
+            return head;
+        }
 
         // dummy head
         ListNode *dummy_head = new ListNode(-1);
@@ -46,18 +65,39 @@ public:
         }
         return dummy_head->next;
     }
+
+    ListNode* removeElementsRecursive(ListNode* head, int val) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        head->next = removeElementsRecursive(head->next, val);
+        return (head->val == val )? head->next : head;
+    }
 };
 
 int main() 
 {
     int arr[] = {1, 2, 6, 3, 4, 5, 6};
-    ListNode *head = new ListNode(std::begin(arr), std::end(arr));
-    std::cout << head << std::endl;
-
     Solution solu;
-    ListNode *result = solu.removeElements(head, 6);
-    std::cout << result << std::endl;
+
+    std::cout << "removeElementsRecursive" << std::endl;
+    ListNode *head = new ListNode(std::begin(arr), std::end(arr));
+    std::cout << "origin: " << head << std::endl;
+    ListNode *result = solu.removeElementsRecursive(head, 6);
+    std::cout << "removed: " << result << std::endl;
     
+    std::cout << "removeElements" << std::endl;
+    head = new ListNode(std::begin(arr), std::end(arr));
+    std::cout << "origin: " << head << std::endl;
+    result = solu.removeElements(head, 6);
+    std::cout << "removed: " << result << std::endl;
+
+    std::cout << "removeElementsWithDummy" << std::endl;
+    head = new ListNode(std::begin(arr), std::end(arr));
+    std::cout << "origin: " << head << std::endl;
+    result = solu.removeElementsWithDummy(head, 6);
+    std::cout << "removed: " << result << std::endl;
 
     return 0;
 }
